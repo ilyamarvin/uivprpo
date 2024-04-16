5dca96ec72232065eedeb9f214eb04678e75a2b0
Ilya Sereda <ilya.sereda2002@mail.ru>
2024-04-16 23:45:21 +0300

    Added integration with Travis CI and Sonar Cloud

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class Main {
+
+    public static int sumTwoDigits(int x, int y) {
+        return x + y;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int result = sumTwoDigits(10, 15);
+        System.out.println(result);
     }
 }
